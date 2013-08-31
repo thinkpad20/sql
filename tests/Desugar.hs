ae140b7ed9ce163d7da00b5869d5779039604989
Allen Nelson <ithinkican@gmail.com>
2013-08-30 22:51:04 -0500

    prototyping desugarer in haskell

---
+++ tests/Desugar.hs
@@ -0,0 +1,160 @@
+module Desugar where
+
+import SRA
+import Data.List
+import qualified Data.Map as M
+import Control.Monad
+
+{------------------- Example queries/tables -------------------}
+tables :: M.Map String [(String, Type)]
+t = [("x", Int), ("y", Int)]
+u = [("x", Int), ("y", Int), ("z", Int)]
+tables = M.fromList [("t", t), ("u", u)]
+
+-- select x, y from t;
+ex0 = Project [(Col "x" Nothing, Nothing), (Col "y" Nothing, Nothing)]
+                  (TableName "t" Nothing)
+
+-- select x, x + y as z from t;
+ex1 = Project [(Col "x" Nothing, Nothing), 
+                (Binary "+" (Col "x" Nothing) (Col "y" Nothing), Just "z")] 
+                  (TableName "t" Nothing)
+
+-- select z from (select x, x+y as z from t);
+ex2 = Project [(Col "z" Nothing, Nothing)] ex1
+
+-- select x, z from (select x, x+y as z from t) where x = z;
+ex3 = Project [(Col "x" Nothing, Nothing), (Col "z" Nothing, Nothing)]
+                (Select (Compare "=" (Col "x" Nothing) (Col "z" Nothing)) ex1)
+
+-- select x, y, z as foo from u;
+ex4 = Project [(Col "x" Nothing, Nothing),
+               (Col "y" Nothing, Nothing),
+               (Col "z" Nothing, Just "foo")]
+                  (TableName "u" Nothing)
+
+-- select * from u;
+ex5 = Project [(Col "*" Nothing, Nothing)] (TableName "u" Nothing)
+
+examples = [ex0, ex1, ex2, ex3, ex4, ex5]
+
+runTests = forM_ examples (putStrLn . show . desugar) 
+
+desugar :: SRA -> RA
+-- desugaring a table name means looking up the table, which
+-- will be a list of (name, type) pairs, and creating a list
+-- of expressions from those pairs, where each expression is
+-- a single column (toExpr)
+desugar (TableName name Nothing) = case M.lookup name tables of
+  Just cols -> Table name cols
+  Nothing -> error $ "No table named " ++ name
+
+-- if there is a table alias given, we wrap our table in a Rho
+-- operator.
+desugar (TableName name (Just n)) = 
+  RhoTable n $ desugar $ TableName name Nothing
+-- desugaring a projection, we need to get the list of expressions
+-- we want to project, and then apply a rho operator for each one
+-- in the list that has an alias. We also need to check for *s.
+desugar (Project nes sra) =
+  let 
+    ra = desugar sra
+    -- get whatever we need to project
+    toProject = (map fst nes)
+    expand :: RA -> [Expression]
+    expand ra = map (\(n,_) -> Col n Nothing) $ getCols ra
+    -- expand any stars
+    starExpand [] = []
+    -- later we'll specifically address t.* vs u.*
+    starExpand ((Col "*" _):es) = expand ra ++ starExpand es
+    starExpand (e:es) = e : starExpand es
+    project = Pi (starExpand toProject) ra
+    doRename :: [NamedExpr] -> RA -> RA
+     -- if no more expressions to do, return RA
+    doRename [] ra = ra
+    -- if we have an expression, but not renamed, no need to add anything
+    doRename ((_, Nothing):nes) ra = doRename nes ra
+    -- if we have a renaming, now we need to apply a rho operator
+    doRename ((e, Just n):nes) ra = Rho e n (doRename nes ra)
+  in
+  doRename nes project
+-- desugaring a select operator is simple
+desugar (Select cond sra) = Sigma cond (desugar sra)
+-- for an Inner join, it's a cross product but it might have a join
+-- condition specified, which would mean a Sigma operator.
+desugar (Join Inner l r jc) = 
+  let (raL, raR) = (desugar l, desugar r) in
+  case jc of
+    Nothing -> Cross raL raR
+    Just cond -> Sigma cond $ Cross raL raR
+
+
+
+-- getCols will return a list of (String, Type) from any table;
+-- a.k.a. the names and types of the columns to be found in this table.
+getCols :: RA -> [Column]
+-- in the simplest case, we already know the answer
+getCols (Table name cols) = cols
+
+-- when we have a project, we are specifying the names of columns to be
+-- carried through. We simply use the show function to get a string
+-- representation of the expression. Table name qualifiers will be kept.
+getCols (Pi exprs ra) = map toCols exprs where
+  toCols expr = (show expr, getType expr ra)
+
+-- to get the columns of a renamed table, we first get the columns
+-- of its subtable. Then, say we're renaming x+y to z. We find the column
+-- called x+y and store it instead as z. Others, we don't touch.
+getCols (Rho e s ra) = 
+  let 
+    cols = getCols ra
+    rename :: Column -> Column
+    rename (cName, t) = if cName == show e then (show e, t) else (cName, t)
+    newCols = map rename cols
+  in
+  newCols
+
+-- renaming a table doesn't change the name of its columns, so we leave it
+getCols (RhoTable _ ra) = getCols ra
+
+-- similarly with a selection
+getCols (Sigma _ ra) = getCols ra
+
+-- in a union or difference, we require the columns be the same, so we make
+-- sure that's the case and if so, we return it.
+getCols (Union l r) =
+  let 
+    colsL = getCols l
+    colsR = getCols r
+  in
+  if colsL == colsR then colsL 
+    else error $ "Mismatched attributes: " ++ show (getCols l) ++ 
+                   " != " ++ show (getCols r)
+
+-- difference is same as union here.
+getCols (Difference l r) = getCols (Union l r)
+
+-- with a cross product, the columns will be concatenated.
+getCols (Cross l r) = getCols l ++ getCols r
+
+
+-- Small type checking function for getCols; also will report type
+-- mismatch errors or non-existent columns.
+getType :: Expression -> RA -> Type
+getType (Col name _) ra = case lookup name (getCols ra) of
+  -- todo: examine cases where table name is specified
+  Just t -> t
+  Nothing -> error $ "Column " ++ name ++ " does not exist" 
+getType (Neg e) ra = case getType e ra of
+  Int -> Int
+  String -> error "Can't apply (-) to a string"
+getType (Binary "||" e1 e2) ra = 
+  case (getType e1 ra, getType e2 ra) of
+    (String, String) -> String
+    otherwise -> error "Can't concat anything but strings"
+getType (Binary op e1 e2) ra = 
+  let 
+    t1 = getType e1 ra
+    t2 = getType e2 ra
+  in
+  if t1 == t2 then t1 else error $ "Mismatched types in " ++ op
---
+++ tests/SRA.hs
@@ -0,0 +1,69 @@
+module SRA where
+
+data Type = Int | String deriving (Show, Eq)
+
+type Column = (String, Type)
+type NamedExpr = (Expression, Maybe String)
+
+-- second string is optional table's name
+data Expression = Col String (Maybe String)
+           | Binary String Expression Expression
+           | Neg Expression
+           deriving (Eq)
+
+data Condition = Bool Bool
+               | Compare String Expression Expression
+               | BinaryCond String Condition Condition
+               | Not Condition
+               deriving (Eq)
+
+data JoinType = Inner
+              | LeftOuter
+              | RightOuter
+              | FullOuter
+              deriving (Eq, Show)
+
+data SRA = TableName String (Maybe String)
+      | Project [NamedExpr] SRA
+      | Select Condition SRA
+      | NaturalJoin SRA SRA
+      | Join JoinType SRA SRA (Maybe Condition)
+      | SRAUnion SRA SRA
+      | Except SRA SRA
+      deriving (Eq)
+
+data RA = Table String [Column]
+      | Pi [Expression] RA
+      | Rho Expression String RA
+      | RhoTable String RA
+      | Sigma Condition RA
+      | Union RA RA
+      | Difference RA RA
+      | Cross RA RA
+      deriving (Show, Eq)
+
+instance Show Expression where
+  show (Col name Nothing) = name
+  show (Col name (Just tname)) = tname ++ "." ++ name
+  show (Binary op ex1 ex2) = show ex1 ++ op ++ show ex2
+  show (Neg ex) = "-(" ++ show ex ++ ")" 
+
+instance Show SRA where
+  show (TableName name Nothing) = name
+  show (TableName name (Just n) ) = name ++ " as " ++ n
+  show (Project nes sra) = "select " ++ show nes ++ " from (" ++ show sra ++ ")"
+  show (Select cond sra) = show sra ++ " where " ++ show cond
+  show (NaturalJoin l r) = "(" ++ show l ++ ") ⋈ (" ++ show r ++ ")"
+  show (Join t l r cond) = "(" ++ show l ++ ") " ++ show t ++ 
+                            " join (" ++ show r ++ ")" ++ c
+    where c = case cond of
+                Nothing -> ""
+                Just co -> " on " ++ show co
+  show (SRAUnion l r) = show l ++ " union " ++ show r
+  show (Except l r) = show l ++ " except " ++ show r
+
+instance Show Condition where
+  show (Bool b) = show b
+  show (Compare op e1 e2) = show e1 ++ op ++ show e2
+  show (BinaryCond op c1 c2) = "(" ++ show c1 ++ op ++ show c2 ++ ")"
+  show (Not c) = "not (" ++ show c ++ ")"
